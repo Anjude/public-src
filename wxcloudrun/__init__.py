16a52a77ce9405a2014a5d407f456560997f7873
Anjude <aboy007262@163.com>
2022-10-01 20:00:50 +0800

    update

--- wxcloudrun/__init__.py
+++ wxcloudrun/__init__.py
@@ -1,3 +1,8 @@
 import pymysql
+import os
+
+os.environ.setdefault('MYSQL_ADDRESS', 'sh-cynosdbmysql-grp-3ze8gwlw.sql.tencentcdb.com:22307')
+os.environ.setdefault('MYSQL_USERNAME', 'root')
+os.environ.setdefault('MYSQL_PASSWORD', 'DMPFDt37')
 
 pymysql.install_as_MySQLdb()
